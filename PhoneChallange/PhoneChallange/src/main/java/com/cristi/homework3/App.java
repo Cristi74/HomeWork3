40c9921b14f33c8c1539ece2166afba40e2d7eaf
cristian.sita <cristian.sita@accesa.com>
2021-04-02 12:18:47 +0300

    Start project

---
+++ PhoneChallange/PhoneChallange/src/main/java/com/cristi/homework3/App.java
@@ -0,0 +1,13 @@
+package com.cristi.homework3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Phones challange" );
+    }
+}
